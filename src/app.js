331b198041cc9a13c28bb845a0169fd0aacc49f3
Birenderjit <birenderjit@gmail.com>
2019-06-06 14:47:18 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,117 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+//console.log(__dirname);
+//console.log(path.join(__dirname, '..', '/public'));
+
+const app = express();
+
+const publicDirPath = path.join(__dirname, '..', '/public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars and the views path
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to use
+app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Marco Polo'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Marco Polo'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: "This is the help page!!",
+    title: 'Help',
+    name: "Marco Polo"
+  });
+});
+
+// app.get('/help', (req, res) => {
+//   res.send('<h1>Help</h1>');
+// });
+
+// app.get('/about', (req, res) => {
+//   res.send('<h1>About</h1>');
+// });
+
+app.get('/weather', (req, res) => {
+
+  if (!req.query.address) {
+    return res.send({
+      error: "Please provide a address to search"
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        address: req.query.address,
+        location,
+        forecast: forecastData
+      });
+    });
+  });
+
+  //console.log(req.query.address);
+
+});
+
+app.get('/products', (req, res) => {
+
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    });
+  }
+  console.log(req.query.search);
+
+  res.send({
+    products: []
+  });
+
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Marco Polo',
+    errorMessage: 'Help article not found.'
+  })
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Marco Polo',
+    errorMessage: 'Page Not found.'
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
---
+++ src/utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+	const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYmlyZW5kZXJqaXQiLCJhIjoiY2p3Y2phdXhrMDY4bzRhcWh4bG1oenJ0MSJ9.Du7I0Hn5Rs14iYeChevNag&limit=1';
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			callback('Unable to connect to location services!', undefined);
+		} else if (body.features.length === 0) {
+			callback('Unable to find location. Try another search.', undefined);
+		} else {
+			callback(undefined, {
+				latitude: body.features[0].center[1],
+				longitude: body.features[0].center[0],
+				location: body.features[0].place_name
+			});
+		}
+	});
+};
+
+module.exports = geocode;
